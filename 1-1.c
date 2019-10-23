87857845355f4f296442e81a3b65a7e93fc1d690
competen110 <814148846@qq.com>
2019-10-23 22:03:05 +0800

    Add files via upload

---
+++ 1-1.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Tom Zhang on 2019/10/23.
+//  Copyright © 2019 Tom Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
